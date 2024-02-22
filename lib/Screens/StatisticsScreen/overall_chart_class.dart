5b9dd13f03cd66b3f0dcd0ab7d8a4cbe82a69e43
Yosranabil <yosraghoneim2002@gmail.com>
2024-02-22 19:32:07 +0200

    Added Weekly & Daily Charts

--- lib/Screens/StatisticsScreen/overall_chart_class.dart
+++ lib/Screens/StatisticsScreen/overall_chart_class.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
 class ChartData {
   ChartData(this.x, this.y);
   final String x;
   final double? y;
 }
 
-final List<ChartData> chartData1 = [
+final List<ChartData> monthlyData1 = [
   ChartData('May', 35),
   ChartData('Jun', 13),
   ChartData('Jul', 34),
@@ -12,10 +15,58 @@ final List<ChartData> chartData1 = [
   ChartData('Sep', 40)
 ];
 
-final List<ChartData> chartData2 = [
+final List<ChartData> monthlyData2 = [
   ChartData('May', 40),
   ChartData('Jun', 9),
   ChartData('Jul', 45),
   ChartData('Aug', 17),
   ChartData('Sep', 10)
 ];
+
+List<ChartData> dailyData1 = [
+  ChartData("01:00", 10),
+  ChartData("03:00", 20),
+  ChartData("06:00", 15),
+  ChartData("09:00", 30),
+  ChartData("12:00", 25)
+];
+
+List<ChartData> dailyData2 = [
+  ChartData("01:00", 15),
+  ChartData("03:00", 10),
+  ChartData("06:00", 5),
+  ChartData("09:00", 35),
+  ChartData("12:00", 40)
+];
+
+List<ChartData> weeklyData1 = [
+  ChartData("Mon", 30),
+  ChartData("Tue", 40),
+  ChartData("Wed", 50),
+  ChartData("Thu", 45),
+  ChartData("Fri", 60)
+];
+
+List<ChartData> weeklyData2 = [
+  ChartData("Mon", 40),
+  ChartData("Tue", 30),
+  ChartData("Wed", 60),
+  ChartData("Thu", 55),
+  ChartData("Fri", 65)
+];
+
+
+List<SplineSeries<ChartData, String>> getSplineSeries(List<ChartData> data, Color color, String name) {
+  return data.map((seriesData) {
+    return SplineSeries<ChartData, String>(
+      dataSource: data,
+      name: name,
+      splineType: SplineType.cardinal,
+      cardinalSplineTension: 0.9,
+      color: color,
+      xValueMapper: (ChartData data, _) => data.x,
+      yValueMapper: (ChartData data, _) => data.y!,
+      markerSettings: const MarkerSettings(isVisible: true),
+    );
+  }).toList();
+}
