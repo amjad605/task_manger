38d408e7c86a5382a4166dc30899bdb7816cc445
IsmailIbrahim5 <ismaeel_ibrahem@outlook.com>
2024-04-14 11:35:02 +0200

    Added - Points Selection - Priority Selection - Description And connected with the API

--- lib/Api/api_servies.dart
+++ lib/Api/api_servies.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 
+import '../Constants/constants.dart';
+import '../cache_helper/local.dart';
+
 class ApiService {
   static const _baseUrl = 'https://cxmanager.onrender.com';
   final Dio _dio = Dio(
@@ -8,17 +11,19 @@ class ApiService {
       receiveDataWhenStatusError: true,
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${CacheHelper.getData(key: kAccessToken)}'
       },
     ),
   );
 
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await _dio.get('$endPoint');
+    var response = await _dio.get(endPoint);
     return response.data;
   }
 
   Future<Map<String, dynamic>> post(
       {required String endPoint, required body}) async {
+
     var response = await _dio.post(endPoint, data: body);
     return response.data;
   }
