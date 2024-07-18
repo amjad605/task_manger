8c500261eaa14f7465116e894fecdd7c53801462
amira_nasser <amira20nasser@gmail.com>
2024-07-18 21:24:38 +0300

    update home screen UI

--- lib/components/task_widget.dart
+++ lib/components/task_widget.dart
@@ -1,129 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:percent_indicator/percent_indicator.dart';
-import 'package:task_manger/screens/add_task_screen/widgets/circle_transition.dart';
-import 'package:task_manger/screens/task_details_screen/task_detail.dart';
-
 import '../Constants/constants.dart';
 
 class TaskWidget extends StatelessWidget {
-  const TaskWidget(
-      {super.key,
-      required this.percent,
-      required this.titleTask,
-      required this.deadlineDate,
-      required this.urlImages});
-  final double percent;
-  final String titleTask, deadlineDate;
-  final List<String> urlImages;
+  final String title;
+  final String? body;
+  final String deadlineDate;
+  final String startDate;
+  final bool isfinished;
+  const TaskWidget({
+    required this.title,
+    this.body,
+    this.isfinished = false,
+    Key? key,
+    required this.deadlineDate,
+    required this.startDate,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        // Get the global position of the tap event
-        final renderBox = (context.findRenderObject() as RenderBox);
-        final offset = renderBox
-            .localToGlobal(Offset(renderBox.size.width, renderBox.size.height));
-
-        //  Navigator.of(context).push(_createRoute(offset));
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            //CircularPrecentage
-            percent == -1
-                ? IconButton(
-                    onPressed: () {}, icon: const Icon(Icons.play_arrow))
-                : Expanded(
-                    flex: 2,
-                    child: CircularPercentIndicator(
-                      animation: true,
-                      rotateLinearGradient: false,
-                      backgroundColor: kMainColor.withOpacity(0.3),
-                      radius: 35,
-                      lineWidth: 3,
-                      percent: percent,
-                      progressColor: kLightblue,
-                      center: Text("${((percent) * 100).toInt()}%"),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10.w),
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: tintBlack,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 250.w,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: isfinished,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                  child: const CircleAvatar(
+                    backgroundColor: kMainColor,
+                    radius: 10,
+                    child: Icon(
+                      Icons.done,
+                      size: 15,
+                      color: Colors.white,
                     ),
                   ),
-
-            const SizedBox(width: 15),
-            //Texts of tasks
-            Expanded(
-              flex: 5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    titleTask,
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.sp),
-                  ),
-                  Text(
-                    "Deadline:$deadlineDate",
-                    style: TextStyle(fontSize: 10.sp),
-                  ),
-                ],
+                ),
               ),
-            ),
-            //  friends
-            Expanded(
-              flex: 2,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  for (int i = 0; i < urlImages.length; i++)
-                    Align(
-                      widthFactor: 1 / urlImages.length,
-                      child: CircleAvatar(
-                        radius: 21,
-                        backgroundColor: Colors.white,
-                        child: CircleAvatar(
-                          radius: 20,
-                          backgroundImage: AssetImage(
-                            urlImages[i],
-                          ),
-                        ),
-                      ),
-                    )
-                ],
+            ],
+          ),
+          const SizedBox(height: 15),
+          Text(
+            body ??
+                """For now, there is no description. You can edit your task and provide a description.""",
+            style:
+                TextStyle(color: Colors.white.withOpacity(.75), fontSize: 14),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "$startDate - ",
+                style: const TextStyle(
+                  color: kMainColor,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
-        ),
+              Text(
+                deadlineDate,
+                style: const TextStyle(
+                  color: kMainColor,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
-}
-
-// Route _createRoute(Offset offset) {
-//   return PageRouteBuilder(
-//     transitionDuration: const Duration(milliseconds: 1000),
-//     pageBuilder: (context, animation, secondaryAnimation) =>
-//         const TaskDetailsScreen(task:  task,),
-//     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//       var begin = Offset(offset.dx, offset.dy);
-//       const end = Offset.zero;
-//       const curve = Curves.ease;
-
-//       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-//       return CircleTransition(
-//         animation: animation,
-//         startingPoint: offset,
-//         startingRadius: 100,
-//         child: child,
-//       );
-//     },
-//   );
-// }
-
-Offset getOffset(GlobalKey key) {
-  final renderBox = (key.currentContext?.findRenderObject() as RenderBox);
-  final offset = renderBox.localToGlobal(
-      Offset(renderBox.size.width / 2, renderBox.size.height / 2));
-  return offset;
 }
